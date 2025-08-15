e16bf32d378b9dc720d9f35baa46853bfe81a733
DanuelDev <danihorvath0@gmail.com>
2025-08-14 21:00:20 -0300

    segundo commit

---
+++ exercicios_bootstrap/14-08/exemplo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro PHP</title>
+</head>
+<body> 
+    <?php # Será processado e transformado em HTML pelo servidor
+        $dia = date('d'); # toda variável começa com $
+
+        # php -S localhost:8080 (comando para iniciar o php no servidor local)
+        # no navegador: localhost:8080/caminho/ate_o/php.php
+    ?>
+    <h1>Hoje é dia <?= $dia?> de Agosto de 2025</h1>
+</body>
+</html>
