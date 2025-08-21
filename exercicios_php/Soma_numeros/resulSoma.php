13c89cf6c1ec8678821f526309009ba4091576cf
DanuelDev <danihorvath0@gmail.com>
2025-08-21 20:55:20 -0300

    mudanca

--- exercicios_php/Soma_numeros/resulSoma.php
+++ exercicios_php/Soma_numeros/resulSoma.php
@@ -8,10 +8,12 @@
 </head>
 <body class="container"> 
 <?php
+    if ($_SERVER['REQUEST_METHOD' == 'POST']){
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
     $soma = $num1 + $num2;
-    echo "<p>Seja bem vindo(a) $soma</p>";
+    echo "<p>Soma: $soma</p>";
+}
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
