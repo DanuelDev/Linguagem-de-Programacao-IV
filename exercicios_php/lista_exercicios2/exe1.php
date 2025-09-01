3f04bc25eb43a301bbde608cee82267bcf94a964
Daniel Horvath <danihorvath0@gmail.com>
2025-09-01 14:56:23 -0300

    commit

--- exercicios_php/lista_exercicios2/exe1.php
+++ exercicios_php/lista_exercicios2/exe1.php
@@ -23,53 +23,53 @@ valor e imprima a posição do menor valor na sequência de entrada-->
     <?php
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $lista = [];
-        $erro = false;
-        $mensagem_erro = "";
+    $lista = [];
+    $erro = false;
+    $mensagem_erro = "";
 
-        // Coletar os números
-        for ($i = 1; $i <=7; $i++){
-            $campo = "num$i";
-            if(isset($_POST[$campo]) && is_numeric($_POST[$campo])){
-                $lista[] = $_POST[$campo];
-            } else {
-                $erro = true;
-                $mensagem_erro = "Por favor, preencha todos os campos com números válidos.";
-                break;
-            }
+    // Coletar os números
+    for ($i = 1; $i <=7; $i++){
+        $campo = "num$i";
+        if(isset($_POST[$campo]) && is_numeric($_POST[$campo])){
+            $lista[] = $_POST[$campo];
+        } else {
+            $erro = true;
+            $mensagem_erro = "Por favor, preencha todos os campos com números válidos.";
+            break;
         }
-        
-        if(!$erro){
-            // Verifica se todos são diferentes
-            if(count($lista) !== count(array_unique($lista))){
-                $mensagem_erro = "Todos os números devem ser diferentes!";
-                $erro = true;
-            } else {
-                // Encontra o menor valor e sua posição
-                $menor_valor = $lista[0];
-                $posicao_menor = 1;
+    }
+    
+    if(!$erro){
+        // Verifica se todos são diferentes
+        if(count($lista) !== count(array_unique($lista))){
+            $mensagem_erro = "Todos os números devem ser diferentes!";
+            $erro = true;
+        } else {
+            // Encontra o menor valor e sua posição
+            $menor_valor = $lista[0];
+            $posicao_menor = 1;
 
-                for($j = 1; $j < count($lista); $j++){
-                    if($lista[$j] < $menor_valor){
-                        $menor_valor = $lista[$j];
-                        $posicao_menor = $j + 1;
-                    }
+            for($j = 1; $j < count($lista); $j++){
+                if($lista[$j] < $menor_valor){
+                    $menor_valor = $lista[$j];
+                    $posicao_menor = $j + 1;
                 }
             }
         }
-        
-        // Exibir resultados ou erros
-        if($erro){
-            echo'<div class="row pt-3 pb-3 text-center">';
-            echo"<h3 class='error-message'>Erro! $mensagem_erro</h3>";
-            echo'</div>';
-        } else {
-            echo'<div class="row pt-3 pb-3 text-center">';
-            echo"<h3>Menor número informado: $menor_valor</h3>";
-            echo"<h3>Posição: $posicao_menor</h3>";
-            echo'</div>';
-        }
     }
+    
+    // Exibir resultados ou erros
+    if($erro){
+        echo'<div class="row pt-3 pb-3 text-center">';
+        echo"<h3 class='error-message'>Erro! $mensagem_erro</h3>";
+        echo'</div>';
+    } else {
+        echo'<div class="row pt-3 pb-3 text-center">';
+        echo"<h3>Menor número informado: $menor_valor</h3>";
+        echo"<h3>Posição: $posicao_menor</h3>";
+        echo'</div>';
+    }
+}
     ?>
 </div>
 
